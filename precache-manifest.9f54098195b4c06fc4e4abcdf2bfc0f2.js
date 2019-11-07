1b96f7a77cb0b0cdb86495f290191622ea7395b3
acarlie <ameliacarlie@gmail.com>
2019-11-07 10:16:01 -0500

    Updates

---
+++ precache-manifest.9f54098195b4c06fc4e4abcdf2bfc0f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298652cf4151ed91f55b7329e1be1c53",
+    "url": "/remember_it/index.html"
+  },
+  {
+    "revision": "151aae03cc7dea302012",
+    "url": "/remember_it/static/css/main.3973164f.chunk.css"
+  },
+  {
+    "revision": "33162643f827634930ac",
+    "url": "/remember_it/static/js/2.8fd168b9.chunk.js"
+  },
+  {
+    "revision": "151aae03cc7dea302012",
+    "url": "/remember_it/static/js/main.b384820a.chunk.js"
+  },
+  {
+    "revision": "0e5c476b41f590c8cf94",
+    "url": "/remember_it/static/js/runtime-main.0a0b624c.js"
+  }
+]);
