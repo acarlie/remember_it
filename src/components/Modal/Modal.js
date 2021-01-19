022083a16c164a28d81a610fef20dc3040ac6aa7
acarlie <ameliacarlie@gmail.com>
2021-01-19 17:41:14 -0500

    fix(modal): fixing closeHandler

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -41,15 +41,15 @@ class Modal extends Component {
     closeHandler = (e) => {
         e.preventDefault();
 
-        if (e.target.id !== 'dialog' || e.target.id === 'close') {
+        if (e.target.id === 'scrim' || e.target.id === 'close') {
             this.closeDialog();
         }
     }
 
     render () {
         return (
-            <div id="scrim" className={this.state.open ? styles['open'] + ' ' + styles['modal__scrim'] : styles['closed'] + ' ' + styles['modal__scrim']} onClick={(e) => this.closeHandler(e)}>
-                <div id="dialog" role="dialog" aria-labelledby="title" aria-describedby="description" className={this.state.open ? styles['open'] + ' ' + styles['modal'] : styles['closed'] + ' ' + styles['modal']}>
+            <div id="scrim" className={this.state.open ? styles['modal--open'] + ' ' + styles['modal__scrim'] : styles['modal--closed'] + ' ' + styles['modal__scrim']} onClick={(e) => this.closeHandler(e)}>
+                <div id="dialog" role="dialog" aria-labelledby="title" aria-describedby="description" className={this.state.open ? styles['modal--open'] + ' ' + styles['modal'] : styles['modal--closed'] + ' ' + styles['modal']}>
                     <h2 id="title" className="heading--4">
                         { this.state.score === 12 &&
                             'You won!'
